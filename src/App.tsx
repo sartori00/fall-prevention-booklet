bdb5e3c7fb29b75d08bc8607ef11e5d0a034a4bc
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2024-11-11 00:02:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from './assets/img1.png';
+import img2 from './assets/img2.png';
+import img3 from './assets/img3.png';
+import img4 from './assets/img4.png';
+import img5 from './assets/img5.png';
+import img6 from './assets/img6.png';
+import img7 from './assets/img7.png';
+import img8 from './assets/img8.png';
+
+function App() {
+  return (
+    <Carousel fade variant="dark">
+      <Carousel.Item>
+        <img src={img1} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img2} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img3} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img4} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img5} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img6} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img7} alt="Logo" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={img8} alt="Logo" />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default App;
